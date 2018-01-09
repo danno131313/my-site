e17e37620e5181f8541e1e6b2fffd7d084905ac1
Danny Kavanagh <dankava13@gmail.com>
2018-01-09 15:27:26 -0800

    change to semantic ui

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,16 @@ fn index() -> Option<NamedFile> {
     NamedFile::open("static/index.html").ok()
 }
 
-#[get("/<file..>")]
+#[get("/semantic/<file..>")]
+fn semantic(file: PathBuf) -> Option<NamedFile> {
+    NamedFile::open(Path::new("semantic/").join(file)).ok()
+}
+
+#[get("/<file..>", rank = 2)]
 fn files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(file)).ok()
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, files]).launch();
+    rocket::ignite().mount("/", routes![index, files, semantic]).launch();
 }
